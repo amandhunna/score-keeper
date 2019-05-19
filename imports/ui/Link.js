28ce1058339fcb6ae30cfef30c524dd20a77b61e
amanjotSinghDhunna <amanjotsingh260@gmail.com>
2019-05-19 20:14:15 +0530

    initial commit

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -4,12 +4,12 @@ import LinksList from './LinksList';
 import PrivateHeader from './PrivateHeader';
 import AddLink from './AddLink';
 import LinksListFilters from './LinksListFilters';
-import Demo from './mui-header';
-
+import Header from './mui-header';
+import Demo from './drawer'
 export default () => {
   return (
     <div>
-      <PrivateHeader title="Your Links"/>
+      <Header />
       <Demo/>
       <div className = 'page-content'>
         <LinksListFilters/>
