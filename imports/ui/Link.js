def54aa1794496bec80b2a3bd12a99855c88c1b6
amanjotSinghDhunna <amanjotsingh260@gmail.com>
2019-05-19 20:08:56 +0530

    muiheader

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -4,11 +4,13 @@ import LinksList from './LinksList';
 import PrivateHeader from './PrivateHeader';
 import AddLink from './AddLink';
 import LinksListFilters from './LinksListFilters';
+import Demo from './mui-header';
 
 export default () => {
   return (
     <div>
       <PrivateHeader title="Your Links"/>
+      <Demo/>
       <div className = 'page-content'>
         <LinksListFilters/>
         <AddLink/>
